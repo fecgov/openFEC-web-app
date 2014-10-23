2d7653bf84341f84a31e591693aaf071d54f9dfd
Theresa Summa <theresa.summa@gsa.gov>
2014-10-23 23:55:54 +0000

    make filter box slidey

--- static/js/modules/filters.js
+++ static/js/modules/filters.js
@@ -7,6 +7,9 @@ var renderFilters = function(e) {
 module.exports = {
     init: function() {
         events.on('bind:browse', renderFilters);
+        $('.filter-header-bar').on('click', function() {
+            $('.filter-field-container').slideToggle();
+        });
 
         // if loaded on a page with filters, init chosen
         $('#candidate-filters select').chosen({width: "100%"});
