762a8ef3ada8fef6d7deadf8cf86a7a8df86ce04
Theresa Summa <theresa.summa@gsa.gov>
2014-10-30 22:04:05 +0000

    changing url on search results

--- static/js/modules/urls.js
+++ static/js/modules/urls.js
@@ -1,7 +1,17 @@
 var events = require('./events.js');
 
 var changeURL = function(context) {
-    window.history.pushState('', '', '/' + context.category);
+    var URL = '/';
+
+    if (typeof context.category !== 'undefined') {
+        URL += context.category;
+
+        if (typeof context.query !== 'undefined') {
+            URL += '?name=' + encodeURIComponent(context.query);
+        }
+    }
+
+    window.history.pushState('', '', URL);
 };
 
 module.exports = {
