a19d420450c2871d1ad22a15abf39bdbb36057b8
Theresa Summa <theresa.summa@gsa.gov>
2014-10-30 21:38:32 +0000

    change active header when viewing search results

--- static/js/modules/navs.js
+++ static/js/modules/navs.js
@@ -15,11 +15,17 @@ var changeActiveNavLink = function(category) {
     $('.header-nav-bar a[name=' + category + ']').addClass('active');
 };
 
+var renderHandler = function(e) {
+    changeActiveNavLink(e.category);
+};
+
 module.exports = {
     init: function() {
         var section = $('#main').data('section');
         $('.header-nav-bar a:not([name=search])').on('click', navClickHandler);
         $('.browse-links a').on('click', navClickHandler);
+
+        events.on('render:browse', renderHandler);
 
         if (typeof section !== 'undefined') {
             changeActiveNavLink(section);
