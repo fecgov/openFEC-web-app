c90c5bc6773539d5e267abe51d276595808414bf
Theresa Summa <theresa.summa@gsa.gov>
2014-11-07 01:03:17 +0000

    prev and next links

--- static/js/modules/tmpls.js
+++ static/js/modules/tmpls.js
@@ -1,3 +1,4 @@
+var urls = require('./urls.js');
 var events = require('./events.js');
 var Handlebars = require('handlebars');
 var candidateHelpers = require('../../../shared/candidate-helpers.js');
@@ -9,9 +10,25 @@ var renderBrowse = function(e) {
 
         promise.done(function(data) {
             var context = {},
-            page = e.data[1].pagination.page;
+                totalPages;
             context.candidates = candidateHelpers.buildCandidateContext(e.data[2].results);
             context.resultsCount = e.data[1].pagination.count;
+            context.page = e.data[1].pagination.page;
+            totalPages = e.data[1].pagination.pages / e.data[1].pagination.per_page;
+             if (typeof e.filters === 'undefined') {
+                e.filters = {};
+            }
+           
+            if (e.data[1].pagination.page < totalPages) {
+                e.filters.page = e.data[1].pagination.page + 1;
+                context.nextURL = urls.buildURL(e);
+            }
+            if (context.page > 1) {
+                e.filters.page = e.data[1].pagination.page + 1;
+                context.prevURL = urls.buildURL(e);
+            }
+            context.perPage = e.data[1].pagination.per_page;
+
             templates[tmplName] = Handlebars.compile(data);
             $('#' + e.category).html(templates[tmplName](context));
             events.emit('bind:browse');
@@ -21,7 +38,7 @@ var renderBrowse = function(e) {
 var renderFilters = function(e) {
     var tmplName = e.category,
         partialName = tmplName + '-table';
-    
+
     // pre-load table partial so the template can be shared on client + server
     $.when(
         loadTemplate('views/' + tmplName + '.handlebars'),
