d156f08e89031b134f590ecbc6f9a39fe66fe2e5
Theresa Summa <theresa.summa@gsa.gov>
2014-11-19 20:22:34 +0000

    implementing committee endpoint changes

--- shared/committee-helpers.js
+++ shared/committee-helpers.js
@@ -18,14 +18,8 @@ module.exports = {
         for (i = 0; i < len; i++) {
             committee = results[i][0];
 
-            if (typeof committee.candidates !== 'undefined') {
-                type = committee.candidates[0].type;
-                designation = committee.candidates[0].designation;
-            }
-            else {
-                type = committee.type;
-                designation = '';
-            }
+            type = committee.status[0].type;
+            designation = committee.status[0].designation;
 
             name = committee.name || '';
             if (typeof committee.treasurer !== 'undefined') {
