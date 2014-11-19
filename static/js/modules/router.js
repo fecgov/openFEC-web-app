44df1e38b5563c4c712a4bfcff31bacc04591784
Theresa Summa <theresa.summa@gsa.gov>
2014-11-19 23:20:50 +0000

    basic routing of table pages

--- static/js/modules/router.js
+++ static/js/modules/router.js
@@ -1,13 +1,31 @@
 'use strict';
 
 var events = require('./events.js');
+var _ = require('underscore');
+var queryString = require('querystring');
+
+var routeToTable = function(category) {
+    var qs = queryString.parse(document.location.search);
+
+    if (_.isEmpty(qs)) {
+        events.emit('load:browse', {
+            'category': category
+        });
+    }
+};
 
 module.exports = {
     init: function() {
+
         window.onpopstate = function() {
+            var queryString = require('querystring');
+
             switch (document.location.pathname) {
                 case '/candidates':
-                    debugger;
+                    routeToTable('candidates');
+                    break;
+                case '/committees':
+                    routeToTable('committees');
                     break;
                 case '/':
                     events.emit('render:landingView');
