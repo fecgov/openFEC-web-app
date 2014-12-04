856cf0a06f63712b58bd7e524a4512bdc8dd4af5
Theresa Summa <theresa.summa@gsa.gov>
2014-12-04 00:42:14 +0000

    browse candidates passing

--- tests/selenium/browse-candidates.js
+++ tests/selenium/browse-candidates.js
@@ -3,6 +3,7 @@ var webdriver = require('selenium-webdriver'),
     chai = require('chai'),
     assert = chai.assert,
     getResultsRows,
+    verifyFirstPage,
     driver = new webdriver.Builder()
     .usingServer(sauce)
     .withCapabilities({
@@ -14,7 +15,44 @@ var webdriver = require('selenium-webdriver'),
         accessKey: process.env.SAUCE_ACCESS_KEY
     })
     .build();
- 
+
+verifyFirstPage = function() {
+    // confirm record count exists and is correct
+    driver.findElement(webdriver.By.css('#results-count p:first-child')).getInnerHtml().then(function(text) {
+        assert.equal(text, 'Results: 22 records');
+    });
+
+    // the table should have one row for each of 20 results + header row
+    // webdriver.By.css doesn't return what I'd expect, so using qSA instead
+    getResultsRows = function() {
+        return document.querySelectorAll('table tr').length;
+    };
+
+    driver.executeScript(getResultsRows).then(function(rows) {
+        assert.equal(rows, 21);
+    });
+
+    // the right data should be output
+    driver.findElements(webdriver.By.css('#results tr:first-child td')).then(function(cols) {
+        var firstRow = [
+            'DODDS, PHILIP',
+            'House',
+            '2012',
+            'Invalid Party Code',
+            'FL',
+            '03'
+        ],
+        i = 0;
+
+        cols.forEach(function(col) {
+            col.getText().then(function(text) {
+                assert.equal(text, firstRow[i]);
+                i++;
+            });
+        });
+    });
+}; 
+
 driver.get('http://localhost');
 driver.wait(function() {
     return driver.findElement(webdriver.By.className('js-initialized'));
@@ -26,33 +64,7 @@ driver.findElement(webdriver.By.css('.browse-links a[name=candidates]')).click()
 // confirm progress bar exists
 driver.wait(function() {
     return driver.findElement(webdriver.By.id('progress'));
-}, 2000);
-
-// confirm record count exists and is correct
-driver.findElement(webdriver.By.css('#results-count p:first-child')).getInnerHtml().then(function(text) {
-    assert.equal(text, 'Results: 22 records');
-});
-
-// the table should have one row for each of 20 results + header row
-getResultsRows = function() {
-    return document.querySelectorAll('table tr').length;
-};
-
-driver.executeScript(getResultsRows).then(function(rows) {
-    assert.equal(rows, 21);
-});
-
-// the right data should be output
-driver.findElement(webdriver.By.css('#results tr:first-child td')).then(function(html) {
-    var firstRow = '<td class="flex-cell">DODDS, PHILIP</td>' +
-                   '<td>House</td>' +
-                   '<td>2012</td>' +
-                   '<td>Invalid Party Code</td>' +
-                   '<td>FL</td>' +
-                   '<td>03</td>';
-
-    assert.equal(firstRow, html);
-});
+}, 2000).then(verifyFirstPage);
 
 // pagination
 driver.findElement(webdriver.By.className('pagination__link')).click();
@@ -62,15 +74,19 @@ driver.wait(function() {
     return driver.findElement(webdriver.By.id('progress'));
 }, 2000);
 
-// wait for load to finish
-driver.wait(function() {
-    return (driver.findElement(webdriver.By.id('progress') == false));
-});
-
 // the results count should update
 // TODO: this is a bug. should actually display 21 - 22
 driver.findElement(webdriver.By.css('#results-count p:last-child')).getInnerHtml().then(function(text) {
     assert.equal(text, 'Viewing: 21 - 40');
 });
+
+// prev link should have right text
+driver.findElement(webdriver.By.className('pagination__link')).getInnerHtml().then(function(text) {
+    assert.equal(text, '<i class="ti-arrow-left"></i> Previous 20');
+});
+
+// click prev link
+driver.findElement(webdriver.By.className('pagination__link')).click()
+    .then(verifyFirstPage);
 
 driver.quit();
