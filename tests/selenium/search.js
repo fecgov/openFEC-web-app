8009fc99571740af16f3800d27ad9a1b9d2e1f6e
Theresa Summa <theresa.summa@gsa.gov>
2014-12-04 23:01:09 +0000

    first pass at adding selenium tests to travis

--- tests/selenium/search.js
+++ tests/selenium/search.js
@@ -16,20 +16,43 @@ var webdriver = require('selenium-webdriver'),
     .build();
  
 driver.get('http://localhost');
+
 driver.wait(function() {
     return driver.findElement(webdriver.By.className('js-initialized'));
 }, 4000);
+
 searchField = driver.findElement(webdriver.By.css('#large-search input[name=search]'));
 searchField.sendKeys('smith');
+
 driver.findElement(webdriver.By.css('#large-search input[type=submit]')).click();
+
 driver.wait(function() {
     return driver.findElement(webdriver.By.id('progress'));
 }, 2000);
+
 driver.wait(function() {
     return driver.findElement(webdriver.By.className('sub-section'));
 }, 2000);
+
 driver.findElement(webdriver.By.tagName('h2')).getInnerHtml().then(function(text) {
     assert.equal(text, 'Search results: <span class="query">smith</span>');
+});
+
+// make sure the nav in header became visisble
+driver.wait(function() {
+    return driver.findElement(webdriver.By.className('header-nav')).isDisplayed();
+}, 1000);
+
+// click "view all" committees
+driver.findElement(webdriver.By.xpath('//*[@id="main"]/div/div[2]/div/a')).click().then(function() {
+    // make sure name filter is populated and active
+    driver.findElement(webdriver.By.xpath('//*[@id="category-filters"]/div[1]')).getAttribute('class').then(function(classes) {
+        assert.equal(classes, 'field active');
+    });
+
+    driver.findElement(webdriver.By.xpath('//*[@id="category-filters"]/div[1]/input')).getAttribute('value').then(function(text) {
+        assert.equal(text, 'smith');
+    });
 });
 
 driver.quit();
