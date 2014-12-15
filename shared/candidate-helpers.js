c79699187dac9d6bb6eda2ee56afc913d1c20850
Noah Manger <noah.manger@gsa.gov>
2014-12-15 15:20:00 -0800

    adding id and incumbent_challenge to results

--- shared/candidate-helpers.js
+++ shared/candidate-helpers.js
@@ -16,15 +16,16 @@ module.exports = {
         for (i = 0; i < len; i++) {
             elections = results[i].elections;
             jlen = elections.length;
-
             for (j = 0; j < jlen; j++) {
                 candidates.push({
                     'name': results[i].name.full_name,
+                    'id': results[i].candidate_id,
+                    'incumbent_challenge': elections[j].incumbent_challenge,
                     'office': elections[j].office_sought,
                     'election': elections[j].election_year,
                     'party': elections[j].party_affiliation,
                     'state': elections[j].state,
-                    'district': elections[j].district
+                    'district': elections[j].district,
                 });
             }
         }
