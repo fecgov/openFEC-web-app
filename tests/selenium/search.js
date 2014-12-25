2ca7648bfeb1b3c1cab727ce6616c3864bc24259
Theresa Summa <theresa.summa@gsa.gov>
2014-12-25 20:11:01 +0000

    h2 check is still firing too early

--- tests/selenium/search.js
+++ tests/selenium/search.js
@@ -40,7 +40,7 @@ driver.wait(function() {
 // results are visible
 driver.wait(function() {
     return driver.findElement(webdriver.By.tagName('h2'));
-}, 20000).then(function() {
+}, 40000).then(function() {
     driver.findElement(webdriver.By.tagName('h2')).getInnerHtml().then(function(text) {
         assert.equal(text, 'Search results: <span class="text--query">smith</span>');
     });
