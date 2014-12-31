81c26fd7accd525f317271d3f0285dca5cdde6fd
Noah Manger <noah.manger@gsa.gov>
2014-12-31 10:44:29 -0800

    Committee suggestions

--- static/js/modules/typeahead.js
+++ static/js/modules/typeahead.js
@@ -1,15 +1,16 @@
 'use strict';
+var Handlebars = require('handlebars');
 
 module.exports = {
   init: function() {
-    var engine = new Bloodhound({
+    // Creating a candidate suggestion engine
+    var candidatesEngine = new Bloodhound({
       name: 'candidates',
       prefetch: {
-        url: "js/data/candidates_2012.json",
+        url: "js/data/candidates_2012.json", // Prefetch 648 candidates from 2012
         filter: function(response) {
           var results = response.results;
           results = $.map(results, function(result) { return {name: result.name.full_name } });
-          console.log('prefetch' + results);
           return results;
         }
       },
@@ -18,7 +19,6 @@ module.exports = {
         filter: function(response) {
           var results = response.results;
           results = $.map(results, function(result) { return {name: result.name.full_name } });
-          console.log(results);
           return results;
         }
       },
@@ -28,7 +28,34 @@ module.exports = {
       },
       queryTokenizer: Bloodhound.tokenizers.whitespace
     });
-    engine.initialize();
+    candidatesEngine.initialize();
+
+    // Committee Engine
+    var committeeEngine = new Bloodhound({
+      name: 'candidates',
+      prefetch: { // Prefetch 2000 Principal committees
+        url: "js/data/committees_p.json",
+        filter: function(response) {
+          var results = response.results;
+          results = $.map(results, function(result) { return {name: result.name } });
+          return results;
+        }
+      },
+      remote: {
+        url: "/rest/committee?name=%QUERY",
+        filter: function(response) {
+          var results = response.results;
+          results = $.map(results, function(result) { return {name: result.name } });
+          return results;
+        }
+      },
+      datumTokenizer: function(d) {
+        var tokens = Bloodhound.tokenizers.whitespace(d.name);
+        return tokens;
+      },
+      queryTokenizer: Bloodhound.tokenizers.whitespace
+    });
+    committeeEngine.initialize();
 
     $('.search-bar').typeahead({
       minLength: 3,
@@ -36,9 +63,22 @@ module.exports = {
       hint: false
     },
     {
+      name: 'Candidates',
       displayKey: 'name',
-      source: engine.ttAdapter()
-    })
+      source: candidatesEngine.ttAdapter(),
+      templates: {
+        header: Handlebars.compile('<h5>Candidates</h5>'),
+      }
+    },
+    {
+      name: 'Committees',
+      displayKey: 'name',
+      source: committeeEngine.ttAdapter(),
+      templates: {
+        header: Handlebars.compile('<h5>Committees</h5>'),
+      }
+    }
+    )
 
     $('.twitter-typeahead').css({
       display: 'block',
