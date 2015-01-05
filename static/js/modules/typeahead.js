60ad097a57e98e5fb2ecbdc866c072eef087e4a5
Noah Manger <noah.manger@gsa.gov>
2015-01-05 15:27:51 -0800

    Refactoring

--- static/js/modules/typeahead.js
+++ static/js/modules/typeahead.js
@@ -2,37 +2,50 @@
 var Handlebars = require('handlebars');
 var events = require('./events.js');
 
-module.exports = {
-  init: function() {
-    function filterCandidates(result) {
-      // Label House, President, Senate
-      var officeFull;
-      switch(result.elections[0].office_sought) {
-        case "H":
-          officeFull = "House of Representatives";
-          break;
-        case "S":
-          officeFull = "Senate";
-          break;
-        case "P":
-          officeFull = "President";
-          break;
-      }
-      var filteredResults =   {
-        name: result.name.full_name,
-        id: result.candidate_id,
-        office: officeFull
-      }
-      return filteredResults;
-    }
+var filterCandidates = function(result) {
 
-    function filterCommittees(result) {
-      var filteredResults =   {
-        name: result.name,
-        id: result.committee_id
-      }
-      return filteredResults;
-    }
+  var officeFull,
+      officeMap,
+      filteredResults;
+  
+  officeMap = {
+    "H": "House of Representatives",
+    "S": "Senate",
+    "P": "President"
+  }
+
+  officeFull = officeMap[result.elections[0].office_sought];
+
+  filteredResults =   {
+    name: result.name.full_name,
+    id: result.candidate_id,
+    office: officeFull
+  }
+  return filteredResults;
+}
+
+var filterCommittees = function(result) {
+  var filteredResults =   {
+    name: result.name,
+    id: result.committee_id
+  }
+  return filteredResults;
+}
+
+// Templates for results
+var candidateSuggestion,
+    committeeSuggestion,
+    headerTpl;
+
+candidateSuggestion = Handlebars.compile('<span>{{ name }} <span class="tt-suggestion__office">{{ office }}</span></span>');
+committeeSuggestion = Handlebars.compile('<span>{{ name }}</span>');
+headerTpl = function(label) {
+  return Handlebars.compile('<span class="tt-dropdown-title">' + label + '</span>');
+}
+
+
+module.exports = {
+  init: function(){
 
     // Creating a candidate suggestion engine
     var candidatesEngine = new Bloodhound({
@@ -63,11 +76,11 @@ module.exports = {
       // dupDetector: function(remoteMatch, localMatch) {
       //   return remoteMatch.name === localMatch.name;
       // },
-      // limit: 5
+      limit: 5
     });
+
     candidatesEngine.clearPrefetchCache();
     candidatesEngine.initialize();
-
 
     // Committee Engine
     var committeeEngine = new Bloodhound({
@@ -98,18 +111,11 @@ module.exports = {
       dupDetector: function(remoteMatch, localMatch) {
         return remoteMatch.name === localMatch.name;
       },
-      limit: 3
+      limit: 5
     });
+
     committeeEngine.clearPrefetchCache();
     committeeEngine.initialize();
-
-    // Templates for results
-    var candidateSuggestion = Handlebars.compile('<span>{{ name }} <span class="tt-suggestion__office">{{ office }}</span></span>');
-    var committeeSuggestion = Handlebars.compile('<span>{{ name }}</span>');
-
-    function headerTpl(label) {
-      return Handlebars.compile('<span class="tt-dropdown-title">' + label + '</span>');
-    }
 
     // Setting up typeahead
     $('.search-bar').typeahead({
@@ -134,8 +140,7 @@ module.exports = {
         suggestion: committeeSuggestion,
         header: headerTpl('Committees'),
       }
-    }
-    )
+    });
 
     $('.twitter-typeahead').css({
       display: 'block',
