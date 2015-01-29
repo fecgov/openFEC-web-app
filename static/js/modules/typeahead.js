9ff35803baca1336d8ad5a8eea8b2966ea192052
Noah Manger <noah.manger@gsa.gov>
2015-01-29 13:46:52 -0800

    Fixing typeahead styling bugs

--- static/js/modules/typeahead.js
+++ static/js/modules/typeahead.js
@@ -91,7 +91,7 @@ module.exports = {
     committeeEngine.initialize();
 
     // Templates for results
-    candidateSuggestion = Handlebars.compile('<span>{{ name }} <span class="tt-suggestion__office">{{ office }}</span></span>');
+    candidateSuggestion = Handlebars.compile('<span><span class="tt-suggestion__name">{{ name }}</span> <span class="tt-suggestion__office">{{ office }}</span></span>');
     committeeSuggestion = Handlebars.compile('<span>{{ name }}</span>');
     headerTpl = function(label) {
       return Handlebars.compile('<span class="tt-dropdown-title">' + label + '</span>');
