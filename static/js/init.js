0306663e6d02f0c7e8f1af601544057ca4422a45
Shawn Allen <shawn.allen@gsa.gov>
2015-02-06 17:27:56 -0800

    add charts module
    add charts module

--- static/js/init.js
+++ static/js/init.js
@@ -2,10 +2,12 @@
 
 var filters = require('./modules/filters.js');
 var typeahead = require('./modules/typeahead.js');
+var charts = require('./modules/charts.js');
 var tablesort = require('tablesort');
 
 filters.init();
 typeahead.init();
+charts.init();
 
 $(document).ready(function() {
 	var $body,
