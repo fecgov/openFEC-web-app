878b3fe6f52fa44fe5096cabb11139be372f1d36
Noah Manger <noah.manger@gsa.gov>
2015-02-23 10:14:10 -0800

    Fixing whitespace

--- static/js/init.js
+++ static/js/init.js
@@ -10,40 +10,38 @@ typeahead.init();
 charts.init();
 
 $(document).ready(function() {
-	var $body,
-		$pageControls;
-
-	$body = $('body');
-	$pageControls = $('.page-controls');
-	
-	$body.addClass('js-initialized');
+    var $body,
+        $pageControls;
+    $body = $('body');
+    $pageControls = $('.page-controls');
+    $body.addClass('js-initialized');
 
     $('.table--sortable').each(function(){
         new tablesort(this);
     });
 
     $('.side-toggle').click(function(){
-    	$('#main').toggleClass('side--open');
+        $('#main').toggleClass('side--open');
     })
 
     // Sticky page controls
     if ( $pageControls.length > 0 ) {
-    	var scrollPos,
-    		controlsHeight,
-    		controlsTop = $pageControls.offset().top;
+        var scrollPos,
+            controlsHeight,
+            controlsTop = $pageControls.offset().top;
 
-	    $(document).scroll(function(){
-	      scrollPos = $(window).scrollTop();
+        $(document).scroll(function(){
+          scrollPos = $(window).scrollTop();
 
-	      if (scrollPos >= controlsTop) {
-	        controlsHeight = $pageControls.height();
-	        $body.addClass('controls--fixed');
-	        $body.css('padding-top', controlsHeight);
-	      } else {
-	        $body.removeClass('controls--fixed');
-	        $body.css('padding-top', 0);
-	      }
-	    })
+          if (scrollPos >= controlsTop) {
+            controlsHeight = $pageControls.height();
+            $body.addClass('controls--fixed');
+            $body.css('padding-top', controlsHeight);
+          } else {
+            $body.removeClass('controls--fixed');
+            $body.css('padding-top', 0);
+          }
+        })
     }
 
     // Reveal containers
