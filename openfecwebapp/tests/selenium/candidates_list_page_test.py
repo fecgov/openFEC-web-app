84d507eeab52980f8418b54e7c7dc953abeb4206
micahsaul <micah.saul@gsa.gov>
2015-03-03 17:42:00 -0800

    Adding committee filter page tests and genericizing filter tests

--- openfecwebapp/tests/selenium/candidates_list_page_test.py
+++ openfecwebapp/tests/selenium/candidates_list_page_test.py
@@ -21,11 +21,51 @@ class CandidatesPageTests(BaseTest):
         return [row.find_elements_by_tag_name('td')[index].text
                 for row in data.find_elements_by_tag_name('tr')]
 
+    def checkFilter(self, name, first_entry, second_entry,
+                    count, index, result):
+        self.driver.get(self.url)
+        self.openFilters()
+        div = self.getFilterDivByName(name)
+        time.sleep(1)
+        div.find_element_by_xpath('./div/a/div/b').click()
+        self.assertEqual(
+            div.find_element_by_xpath('./div').get_attribute('class'),
+            ('chosen-container chosen-container-single '
+             'chosen-with-drop chosen-container-active'))
+        div.find_element_by_tag_name('input').send_keys(first_entry)
+        self.assertEqual(
+            len(div.find_elements_by_tag_name('li')),
+            count)
+        div.find_element_by_tag_name('input').send_keys(second_entry)
+        div.find_element_by_tag_name('input').send_keys(Keys.ENTER)
+        self.assertEqual(
+            div.get_attribute('class'),
+            'field active')
+        div.find_element_by_tag_name('input').send_keys(Keys.ENTER)
+        results = (self.driver.find_elements_by_tag_name('tr'))
+        col = [y.find_elements_by_tag_name('td')[index]
+                .text for y in results[1:]]
+        self.assertEqual(
+            set(col),
+            {result})
+
     def testCandidatesPageLoads(self):
         self.driver.get(self.url)
         self.assertEqual(
             self.driver.find_element_by_tag_name('h1').text,
             'Browse candidate records')
+
+    def testCandidateSorting(self):
+        self.driver.get(self.url)
+        table = self.driver.find_element_by_tag_name('table')
+        headers = (table.find_element_by_tag_name('thead')
+                   .find_elements_by_tag_name('th'))
+        data = table.find_element_by_tag_name('tbody')
+        for i in range(len(headers)):
+            headers[i].click()
+            self.assertEqual(
+                sorted(self.getColumn(i, data), reverse=True),
+                self.getColumn(i, data))
 
     def testCandidatesFilterSideBar(self):
         self.driver.get(self.url)
@@ -49,130 +89,17 @@ class CandidatesPageTests(BaseTest):
             'ALLIEGRO, MARK C')
 
     def testCandidateCycleFilter(self):
-        self.driver.get(self.url)
-        self.openFilters()
-        cycle = self.getFilterDivByName('year')
-        time.sleep(1)
-        cycle.find_element_by_xpath('./div/a/div/b').click()
-        self.assertEqual(
-            cycle.find_element_by_xpath('./div').get_attribute('class'),
-            ('chosen-container chosen-container-single '
-             'chosen-with-drop chosen-container-active'))
-        cycle.find_element_by_tag_name('input').send_keys('201')
-        self.assertEqual(
-            len(cycle.find_elements_by_tag_name('li')),
-            5)
-        cycle.find_element_by_tag_name('input').send_keys('4')
-        cycle.find_element_by_tag_name('input').send_keys(Keys.ENTER)
-        self.assertEqual(
-            cycle.get_attribute('class'),
-            'field active')
-        cycle.find_element_by_tag_name('input').send_keys(Keys.ENTER)
-        results = (self.driver.find_elements_by_tag_name('tr'))
-        yrs = [y.find_elements_by_tag_name('td')[2].text for y in results[1:]]
-        self.assertEqual(
-            set(yrs),
-            {'2014'})
+        self.checkFilter('year', '201', '4', 5, 2, '2014')
 
     def testCandidatePartyFilter(self):
-        self.driver.get(self.url)
-        self.openFilters()
-        party = self.getFilterDivByName('party')
-        time.sleep(1)
-        party.find_element_by_xpath('./div/a/div/b').click()
-        self.assertEqual(
-            party.find_element_by_xpath('./div').get_attribute('class'),
-            ('chosen-container chosen-container-single '
-             'chosen-with-drop chosen-container-active'))
-        party.find_element_by_tag_name('input').send_keys('demo')
-        self.assertEqual(
-            len(party.find_elements_by_tag_name('li')),
-            5)
-        party.find_element_by_tag_name('input').send_keys('cratic party')
-        party.find_element_by_tag_name('input').send_keys(Keys.ENTER)
-        party.find_element_by_tag_name('input').send_keys(Keys.ENTER)
-        results = (self.driver.find_elements_by_tag_name('tr'))
-        ptys = [p.find_elements_by_tag_name('td')[3].text for p in results[1:]]
-        self.assertEqual(
-            set(ptys),
-            {'Democratic Party'})
+        self.checkFilter(
+            'party', 'demo', 'cratic party', 5, 3, 'Democratic Party')
 
     def testCandidateStateFilter(self):
-        self.driver.get(self.url)
-        self.openFilters()
-        state = self.getFilterDivByName('state')
-        time.sleep(1)
-        state.find_element_by_xpath('./div/a/div/b').click()
-        self.assertEqual(
-            state.find_element_by_xpath('./div').get_attribute('class'),
-            ('chosen-container chosen-container-single '
-             'chosen-with-drop chosen-container-active'))
-        state.find_element_by_tag_name('input').send_keys('W')
-        self.assertEqual(
-            len(state.find_elements_by_tag_name('li')),
-            4)
-        state.find_element_by_tag_name('input').send_keys('est')
-        state.find_element_by_tag_name('input').send_keys(Keys.ENTER)
-        state.find_element_by_tag_name('input').send_keys(Keys.ENTER)
-        results = (self.driver.find_elements_by_tag_name('tr'))
-        sts = [p.find_elements_by_tag_name('td')[4].text for p in results[1:]]
-        self.assertEqual(
-            set(sts),
-            {'WV'})
+        self.checkFilter('state', 'w', 'est', 4, 4, 'WV')
 
     def testCandidateDistrictFilter(self):
-        self.driver.get(self.url)
-        self.openFilters()
-        district = self.getFilterDivByName('district')
-        time.sleep(1)
-        district.find_element_by_xpath('./div/a/div/b').click()
-        self.assertEqual(
-            district.find_element_by_xpath('./div').get_attribute('class'),
-            ('chosen-container chosen-container-single '
-             'chosen-with-drop chosen-container-active'))
-        district.find_element_by_tag_name('input').send_keys('1')
-        self.assertEqual(
-            len(district.find_elements_by_tag_name('li')),
-            10)
-        district.find_element_by_tag_name('input').send_keys('0')
-        district.find_element_by_tag_name('input').send_keys(Keys.ENTER)
-        district.find_element_by_tag_name('input').send_keys(Keys.ENTER)
-        results = (self.driver.find_elements_by_tag_name('tr'))
-        dsts = [p.find_elements_by_tag_name('td')[5].text for p in results[1:]]
-        self.assertEqual(
-            set(dsts),
-            {'10'})
+        self.checkFilter('district', '1', '0', 10, 5, '10')
 
     def testCandidateOfficeFilter(self):
-        self.driver.get(self.url)
-        self.openFilters()
-        office = self.getFilterDivByName('office')
-        time.sleep(1)
-        office.find_element_by_xpath('./div/a/div/b').click()
-        self.assertEqual(
-            office.find_element_by_xpath('./div').get_attribute('class'),
-            ('chosen-container chosen-container-single '
-             'chosen-with-drop chosen-container-active'))
-        office.find_element_by_tag_name('input').send_keys('P')
-        self.assertEqual(
-            len(office.find_elements_by_tag_name('li')),
-            1)
-        office.find_element_by_tag_name('input').send_keys(Keys.ENTER)
-        office.find_element_by_tag_name('input').send_keys(Keys.ENTER)
-        results = (self.driver.find_elements_by_tag_name('tr'))
-        ofcs = [p.find_elements_by_tag_name('td')[1].text for p in results[1:]]
-        self.assertEqual(
-            set(ofcs),
-            {'President'})
-
-    def testCandidateSorting(self):
-        self.driver.get(self.url)
-        table = self.driver.find_element_by_tag_name('table')
-        headers = (table.find_element_by_tag_name('thead')
-                   .find_elements_by_tag_name('th'))
-        data = table.find_element_by_tag_name('tbody')
-        for i in range(len(headers)):
-            headers[i].click()
-            self.assertEqual(
-                sorted(self.getColumn(i, data), reverse=True),
-                self.getColumn(i, data))
+        self.checkFilter('office', 'P', '', 1, 1, 'President')
