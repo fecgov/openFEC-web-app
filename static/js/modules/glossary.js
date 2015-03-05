a83425ca1b7ad90c459825c70f8276b77a9e3883
Noah Manger <noah.manger@gsa.gov>
2015-03-05 15:25:19 -0800

    Refactoring glossary definition searching
    And adding in keyboard control

--- static/js/modules/glossary.js
+++ static/js/modules/glossary.js
@@ -1,26 +1,37 @@
 'use strict';
 var terms = require('./terms');
+var _ = require('underscore');
 
 var glossaryLink = $('.term'),
     glossaryIsOpen = false,
+    findDefinition,
     setDefinition,
     showGlossary,
     hideGlossary,
     defineTerm,
     showDefinition;
 
+// Indexing the terms
+terms = _.indexBy(terms, 'term');
+
+// Looks through the terms array to find the definition
+findDefinition = function(term){
+    var definition;
+    if ( terms.hasOwnProperty(term) ) {
+        definition = terms[term].definition;
+    } else {
+        definition = null;
+    }
+    return definition;
+}
+
 // Assigns the correct data-definition to each <span class="term">
 setDefinition = function(){
     $('.term').each(function(){
         var term = $(this).data('term'),
             definition;
-        $(this).attr('title', 'Click to define');
-        for ( var i = 0; i < terms.length; i++ ) {
-            if ( terms[i].term === term ) {
-                definition = terms[i].definition;
-                $(this).attr('data-definition', definition);
-            }
-        }
+        definition = findDefinition(term);
+        $(this).attr('title', 'Click to define').attr('data-definition', definition);
     })
 }
 
@@ -29,6 +40,7 @@ showGlossary = function() {
     $('.side-panel--right').addClass('side-panel--open');
     $('body').addClass('panel-active--right');
     $('#glossary-toggle').addClass('active');
+    $('#glossary-search').focus();
     glossaryIsOpen = true;
 }
 
@@ -70,6 +82,19 @@ module.exports = {
             glossaryIsOpen = false;
         } else {
             showGlossary();
+        }
+    });
+
+    $("#glossary-search").keyup(function(event){
+        if(event.keyCode == 13){
+            var value = $(this).val(),
+                definition;
+            definition = findDefinition(value);
+            if ( definition !== null ) {
+                defineTerm(value, definition);
+            } else {
+                $('#glossary-definition').html('Sorry, there are no terms matching your search: "<strong>' + value + '</strong>". Please try again.');
+            }
         }
     });
  },
