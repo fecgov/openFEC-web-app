5f46ec1de307471b0936b84b2d4b08d70e54d5fb
Theresa Summa <theresa.summa@gsa.gov>
2015-03-18 19:58:58 +0000

    making js work, adding more env vars

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 # usage:
-# ./deploy.sh space-name app-name
+# ./deploy.sh space-name app-name web-username web-password
 # You must be logged into Cloud Foundry
 # You must also follow the npm installation instructions here:
 # https://github.com/18F/openFEC-web-app#installing
 # if you haven't already set this up in your environment
 
 api_url=""
+debug=""
 
 if [ -z "$1" ] || [ -z "$2" ]; then
-  echo "Please pass in the name of the Cloud Foundry app you want to"
-  echo "and the space it is in"
+  echo "Please pass in the name of the Cloud Foundry app you want,"
+  echo "the space it is in, and the HTTP username and password"
   echo "Example: './deploy.sh dev-web fec-web-dev'"
   echo "Run 'cf spaces' to see possible spaces."
   echo "Run 'cf apps' to see possible apps within your current space."
@@ -19,9 +20,10 @@ if [ -z "$1" ] || [ -z "$2" ]; then
 fi
 
 if [ $2 = "fec-web-dev" ]; then
-  api_url="http://fec-api-dev.cf.18f.us/"
+  api_url="http://fec-api-dev.cf.18f.us"
+  debug="true"
 elif [ $2 = "fec-web-stage" ]; then
-  api_url="http://fec-api-stage.cf.18f.us/"
+  api_url="http://fec-api-stage.cf.18f.us"
 else
   api_url="prod"
 fi
@@ -39,6 +41,11 @@ cf target -o fec -s "$1"
 echo " "
 echo "Setting Cloud Foundry environment variables..."
 cf set-env "$2" FEC_WEB_API_URL "$api_url"
+cf set-env "$2" FEC_WEB_DEBUG "$debug"
+# the username and password should be the same for both the
+# web app and API
+cf set-env "$2" FEC_WEB_USERNAME "$3"
+cf set-env "$2" FEC_WEB_PASSWORD "$4"
 echo " "
 echo "Pushing to Cloud Foundry..."
 echo " "
