dc70324607a05a2ae8b5269b3bb7f0d5d1eafc3f
Theresa Summa <theresa.summa@gsa.gov>
2015-04-09 20:33:01 +0000

    adds an env var to set in test and build environments to turn off http auth - also increases the selenium test window sizes to prevent some   incorrect failures

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from openfecwebapp.config import (port, debug, host, api_location,
-    username, password, debug)
+    username, password, test)
 from flask import Flask, render_template, request
 from flask.ext.basicauth import BasicAuth
 from openfecwebapp.views import (render_search_results, render_table,
@@ -16,9 +16,10 @@ app = Flask(__name__)
 
 app.jinja_env.globals['api_location'] = api_location
 
-app.config['BASIC_AUTH_USERNAME'] = username
-app.config['BASIC_AUTH_PASSWORD'] = password
-app.config['BASIC_AUTH_FORCE'] = True
+if not test:
+    app.config['BASIC_AUTH_USERNAME'] = username
+    app.config['BASIC_AUTH_PASSWORD'] = password
+    app.config['BASIC_AUTH_FORCE'] = True
 
 basic_auth = BasicAuth(app)
 
