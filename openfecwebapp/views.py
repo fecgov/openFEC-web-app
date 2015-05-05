8f2b76a28425c61c87922ef9fe6c66e63d553b55
arowla <alison.rowland@gsa.gov>
2015-05-05 10:42:06 -0400

    Removed financial field mappings. HULK SMASH!

--- openfecwebapp/views.py
+++ openfecwebapp/views.py
@@ -1,11 +1,10 @@
 from flask import render_template
-from marshmallow import pprint
 from openfecwebapp.models.candidates import CandidateSchema
 from openfecwebapp.models.committees import CommitteeSchema
 from openfecwebapp.models.shared import generate_pagination_values
-from openfecwebapp.models.financial_summaries import CommitteeFinancials
 from openfecwebapp.api_caller import load_cmte_financials
 from werkzeug.exceptions import abort
+
 
 def render_search_results(results, query):
     candidates = []
@@ -23,7 +22,8 @@ def render_search_results(results, query):
     no_results = True if not len(candidates) and not len(committees) else False
 
     return render_template('search-results.html', candidates=candidates,
-        committees=committees, query=query, no_results=no_results)
+                           committees=committees, query=query, no_results=no_results)
+
 
 # loads browse tabular views
 def render_table(data_type, results, params):
@@ -52,22 +52,24 @@ type_map = {
     'committee': lambda x: CommitteeSchema().dump(x).data
 }
 
+
 def render_page(data_type, *args, **kwargs):
     c_data = args[0]
-    # not handling error at api module because sometimes its ok to 
+    # not handling error at api module because sometimes its ok to
     # not get data back - like with search results
-    if not 'results' in c_data or not c_data['results']:
+    if 'results' not in c_data or not c_data['results']:
         abort(500)
 
     data = c_data['results'][0]
 
-    if data_type == 'candidate': 
+    if data_type == 'candidate':
         candidate_schema = CandidateSchema()
         # candidate fields will be top-level in the template
         tmpl_vars = candidate_schema.dump(data).data
 
         # process related committees
-        committee_fields = ['committee_id', 'name', 'designation_full', 'designation', 'committee_type_full', 'committee_type', 'url', 'is_primary', 'is_authorized']
+        committee_fields = ['committee_id', 'name', 'designation_full', 'designation',
+                            'committee_type_full', 'committee_type', 'url', 'is_primary', 'is_authorized']
         committees = CommitteeSchema(only=committee_fields, many=True, skip_missing=True, strict=True) \
             .dump(kwargs['committees']).data
 
@@ -75,7 +77,7 @@ def render_page(data_type, *args, **kwargs):
 
         # add 'committees' level to template
         for committee in committees:
-            committee['financials'] = load_and_format_cmte_financials(committee.committee_id)
+            committee['financials'] = load_cmte_financials(committee['committee_id'])
 
         tmpl_vars['committees'] = committees
 
@@ -90,20 +92,10 @@ def render_page(data_type, *args, **kwargs):
         tmpl_vars['candidates'] = CandidateSchema(only=['candidate_id', 'name'], many=True, skip_missing=True, strict=True) \
             .dump(kwargs['candidates']).data
 
-        financials_schema = CommitteeFinancials(skip_missing=True, strict=True)
-        results, errors = financials_schema.dump(load_cmte_financials(committee['committee_id']))
-        pprint(errors)
-
-        tmpl_vars.update(results)
+        financials = load_cmte_financials(committee['committee_id'])
+        tmpl_vars['reports'] = financials['reports']
+        tmpl_vars['totals'] = financials['totals']
     else:
         tmpl_vars = data
 
-    pprint(tmpl_vars)
     return render_template(data_type + 's-single.html', **tmpl_vars)
-
-def load_and_format_cmte_financials(committee_id):
-    fin_data = load_cmte_financials(committee_id)
-    schema = CommitteeFinancials()
-    data = schema.dump(fin_data).data
-    return data
-    
