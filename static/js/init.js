e25a9747a2e111844932e637e0fe42a902ec60ff
Noah Manger <noah.manger@gsa.gov>
2015-05-05 17:03:19 -0700

    Consolidating _utitlities and _states

--- static/js/init.js
+++ static/js/init.js
@@ -83,7 +83,7 @@ $(document).ready(function() {
     if (typeof window.sessionStorage !== 'undefined') {
         if (window.sessionStorage.getItem('keep-banner-closed') === '1') {
             $('#notice').attr('aria-hidden', true);
-            $('#notice-reveal').addClass('visible');
+            $('#notice-reveal').addClass('u-visible');
         } else {
             $('#notice').attr('aria-hidden', false);
         }
@@ -91,7 +91,7 @@ $(document).ready(function() {
 
     $("#notice-close").on('click keypress', function(e){
         if (e.which === 13 || e.type === 'click') {
-            $('#notice-reveal').addClass('visible');
+            $('#notice-reveal').addClass('u-visible');
             if (typeof window.sessionStorage !== 'undefined') {
                 window.sessionStorage.setItem('keep-banner-closed', '1');
             }            
@@ -100,7 +100,7 @@ $(document).ready(function() {
 
     // Hide the notice reveal link if you open it 
     $('#notice-reveal').click(function(){
-      $(this).removeClass('visible');
+      $(this).removeClass('u-visible');
     });
 
     // Initialize accordions
