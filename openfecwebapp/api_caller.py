0d9fde286f8f2d93ab35b9196f72506c2b2b26ee
arowla <alison.rowland@gsa.gov>
2015-05-06 22:22:24 -0400

    Changed building of API paths
    - Now in the style of (*args, **kwargs), _call_api will build up path
      components from the positional arguments and run `os.path.join` on them, and
      filters are passed in as keyword arguments

--- openfecwebapp/api_caller.py
+++ openfecwebapp/api_caller.py
@@ -9,12 +9,15 @@ from openfecwebapp.config import api_location, api_version, api_key
 MAX_FINANCIALS_COUNT = 4
 
 
-def _call_api(path, filters={}):
+def _call_api(*path_parts, **filters):
     if api_key:
         filters['api_key'] = api_key
 
-    path = os.path.join(api_version, path.strip('/'))
+    path = os.path.join(api_version, *[x.strip('/') for x in path_parts])
     url = parse.urljoin(api_location, path)
+
+    print(url)
+    print(filters)
 
     results = requests.get(url, params=filters)
 
@@ -37,28 +40,22 @@ def load_search_results(query):
 def load_single_type_summary(data_type, filters):
     url = '/' + data_type
     filters['per_page'] = 30
-    return _call_api(url, filters)
+    return _call_api(url, **filters)
 
 
 def load_single_type(data_type, c_id, filters):
-    return _call_api(os.path.join(data_type, c_id), filters)
+    return _call_api(data_type, c_id, **filters)
 
 
 def load_nested_type(parent_type, c_id, nested_type):
-    url = os.path.join(parent_type, c_id, nested_type)
-    filters = {'year': '*'}
-
-    return _call_api(url, filters)
+    return _call_api(parent_type, c_id, nested_type, year='*')
 
 
 def load_cmte_financials(committee_id):
-    r_url = os.path.join('committee', committee_id, 'reports')
-    t_url = os.path.join('committee', committee_id, 'totals')
-
     filters = {'per_page': MAX_FINANCIALS_COUNT}
 
-    reports = _call_api(r_url, filters)
-    totals = _call_api(t_url, filters)
+    reports = _call_api('committee', committee_id, 'reports', **filters)
+    totals = _call_api('committee', committee_id, 'totals', **filters)
 
     return {
         'reports': reports['results'],
@@ -67,7 +64,7 @@ def load_cmte_financials(committee_id):
 
 
 def load_election_years(candidate_id):
-    candidate = _call_api('/candidate/' + candidate_id)
+    candidate = _call_api('/candidate/', candidate_id)
     return candidate.get('election_years', [])
 
 
