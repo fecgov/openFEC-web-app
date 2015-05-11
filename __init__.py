b8b1759eb60466a7b677529a41db58e50ca53bb5
arowla <alison.rowland@gsa.gov>
2015-05-11 13:29:26 -0400

    Removed chart fake data and Flake8'd the file

--- __init__.py
+++ __init__.py
@@ -84,12 +84,14 @@ def search():
         candidates, committees = load_search_results(query)
         return render_search_results(candidates, committees, query)
     else:
-        return render_template('search.html');
+        return render_template('search.html')
+
 
 @app.route('/candidate/<c_id>/<cycle>')
 def candidate_page_with_cycle(c_id, cycle):
     data = load_single_type('candidate', c_id, {'year': cycle})
     return render_candidate(data)
+
 
 @app.route('/candidate/<c_id>')
 def candidate_page(c_id):
@@ -97,11 +99,13 @@ def candidate_page(c_id):
     committee_data = load_nested_type('candidate', c_id, 'committees')['results']
     return render_candidate(data, committees=committee_data)
 
+
 @app.route('/committee/<c_id>')
 def committee_page(c_id):
     data = load_single_type('committee', c_id, {})
     candidate_data = load_nested_type('committee', c_id, 'candidates')['results']
     return render_committee(data, candidates=candidate_data)
+
 
 @app.route('/candidates')
 def candidates():
@@ -109,32 +113,29 @@ def candidates():
     results = load_single_type_summary('candidates', params)
     return render_table('candidates', results, params)
 
+
 @app.route('/committees')
 def committees():
     params = _convert_to_dict(request.args)
     results = load_single_type_summary('committees', params)
     return render_table('committees', results, params)
 
-@app.route('/charts')
-def charts():
-    data = {}
-    data['grouped_bar_data'] = [{"cash_on_hand": 217341.3, "debts_owed": 0.0, "disbursements": 78988.83, "receipts": 110402.55, "date": "Q1 - 2012"}, {"cash_on_hand": 208841.3, "debts_owed": 0.0, "disbursements": 78988.83, "receipts": 101902.55, "date": "Q1 - 2012"}, {"cash_on_hand": 185927.58, "debts_owed": 0.0, "disbursements": 10926.53, "receipts": 8555.0, "date": "Q1 - 2012"}, {"cash_on_hand": 232215.95, "debts_owed": 0.0, "disbursements": 28688.71, "receipts": 45715.0, "date": "Q1 - 2012"}, {"cash_on_hand": 188299.11, "debts_owed": 0.0, "disbursements": 87884.86, "receipts": 55309.91, "date": "Q1 - 2012"}, {"cash_on_hand": 216189.66, "debts_owed": 0.0, "disbursements": 172159.64, "receipts": 179508.0, "date": "Q1 - 2012"}, {"cash_on_hand": 215189.66, "debts_owed": 0.0, "disbursements": 172159.64, "receipts": 178508.0, "date": "Q1 - 2012"}, {"cash_on_hand": 204669.17, "debts_owed": 520.18, "disbursements": 58153.97, "receipts": 62568.4, "date": "Q1 - 2012"}]
-    data['bar_data'] = map(lambda d: {'debt': d['cash_on_hand']}, data['grouped_bar_data'])
-    data['committee'] = {'totals': {'receipts': 500000, 'disbursements': 450000}}
-    return render_template('charts.html', **data)
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
+
 @app.errorhandler(500)
 def server_error(e):
     return render_template('500.html'), 500
+
 
 @app.template_filter('currency')
 def currency_filter(num, grouping=True):
     if num is not None:
         return locale.currency(num, grouping=grouping)
+
 
 @app.template_filter('date_sm')
 def date_filter_sm(date_str):
@@ -143,10 +144,12 @@ def date_filter_sm(date_str):
     d = datetime.datetime.strptime(date_str, '%a, %d %b %Y %H:%M:%S %z')
     return d.strftime('%m/%y')
 
+
 @app.template_filter()
 def fmt_year_range(year):
     if year is not None and type(year) == 'int':
         return "{} - {}".format(year - 1, year)
+
 
 @app.template_filter()
 def fmt_report_desc(report_full_description):
