420ce694c87b249a2b1df54ae8dc714a0c796025
Joshua Carp <jm.carp@gmail.com>
2015-06-03 19:09:12 -0400

    Move HTML markup for "None" to macro.
    Putting HTML in a string literal in the `currency` filter was breaking
    tests. It's also not ideal, since it requires using the `safe` filter,
    and unsafe content could potentially leak through. This patch restores
    the behavior of the `currency` filter and moves the markup for the "None"
    element to a macro.

--- __init__.py
+++ __init__.py
@@ -176,8 +176,7 @@ def server_error(e):
 def currency_filter(num, grouping=True):
     if isinstance(num, (int, float)):
         return locale.currency(num, grouping=grouping)
-    else:
-        return "<a class='term' data-term='None'>None</a>"
+    return None
 
 
 def _unique(values):
