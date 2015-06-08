3b7ff751bbe7ceca70efd6175282865db268e813
arowla <alison.rowland@gsa.gov>
2015-06-08 16:30:40 -0400

    Fixed syntax errors

--- tests/selenium/single_candidate_page.py
+++ tests/selenium/single_candidate_page.py
@@ -20,11 +20,8 @@ class SingleCandidatePageTests(BaseTest):
 
     def testElectionCycleChooser(self):
         self.driver.get(self.url)
-        self.assertEqual(
-           self.driver.find_element_by_css_selector('select[name=election_cycle] .chosen-container .chosen-single span').text,
-            '2011 - 2012'))
-        // election cycle chooser drop down arrow
+        self.assertEqual(self.driver.find_element_by_css_selector('select[name=election_cycle] .chosen-container .chosen-single span').text, '2011 - 2012')
+        # election cycle chooser drop down arrow
         self.driver.find_element_by_xpath('//*[@id="main"]/div/header/div/div[1]/div[2]/div/a/div').click()
         self.assertEqual(
-            self.driver.find_element_by_xpath('//*[@id="main"]/div/header/div/div[1]/div[2]/div/div/ul/li[2]'.text,
-            '2007 - 2008'))
+            self.driver.find_element_by_xpath('//*[@id="main"]/div/header/div/div[1]/div[2]/div/div/ul/li[2]'.text, '2007 - 2008'))
