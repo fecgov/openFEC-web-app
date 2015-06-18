2325fae4ae74f272d2c02955db973901d161d42f
Noah Manger <noah.manger@gsa.gov>
2015-06-17 17:39:54 -0700

    Showing selected items above dropdowns

--- static/js/init.js
+++ static/js/init.js
@@ -116,6 +116,21 @@ $(document).ready(function() {
     });
 
     $('.dropdown__list').perfectScrollbar({ 'suppressScrollX': true });
+
+    $('.dropdown__list input[type="checkbox"]').change(function(){
+        var checkedBoxes = $(this).parents('ul').find('input:checked').parents('li');
+        var checkedCount = checkedBoxes.length;
+        var listID = $(this).parents('ul').attr('id');
+        var parentButton = $('button[data-toggles="' + listID + '"]');
+        $(this).parents('.dropdown__container').find('.dropdown__selected').prepend(checkedBoxes);
+
+    })
+
+    $('.dropdown__container').each(function(){
+        var $checkedBoxes = $(this).find('.dropdown__list input:checked').parents('li');
+        $(this).find('.dropdown__selected').append($checkedBoxes);
+    })
+
     $(document.body).on('keyup', function(e) {
         if (e.keyCode == keyboard.key.code('escape')) {
             var menu = $('#site-menu');
