5a187c610c5b0c32824722a20bf4fd3f5cf56afb
Joshua Carp <jm.carp@gmail.com>
2015-07-22 11:58:45 -0400

    Restore missing column.

--- static/js/pages/committees.js
+++ static/js/pages/committees.js
@@ -19,6 +19,7 @@ var columns = [
   {data: 'treasurer_name', className: 'min-desktop'},
   {data: 'state', className: 'min-desktop', width: '60px'},
   {data: 'party_full', className: 'min-desktop'},
+  tables.dateColumn({data: 'first_file_date', className: 'min-tablet'}),
   {data: 'committee_type_full', className: 'min-tablet'},
   {data: 'designation_full', className: 'min-tablet'},
   {data: 'organization_type_full', className: 'min-desktop'},
