8664f18101cf16b71e836107e90e04e7242f83e0
Joshua Carp <jm.carp@gmail.com>
2015-08-06 17:39:40 -0400

    Handle stale element errors in Chrome.

--- tests/selenium/utils.py
+++ tests/selenium/utils.py
@@ -11,12 +11,28 @@ def wait_for_ajax(driver, timeout=10, interval=0.1):
         elapsed += interval
 
 
+def wait_for_event(driver, name, label, timeout=10, interval=0.1):
+    elapsed = 0
+    driver.execute_script('window.{0} = false'.format(label))
+    driver.execute_script('''
+        $(document).on("{0}", function() {{
+            window.{1} = true;
+        }});
+    '''.format(name, label))
+    while True:
+        active = driver.execute_script('return window.{0} === true'.format(label))
+        if active or elapsed > timeout:
+            break
+        time.sleep(interval)
+        elapsed += interval
+    driver.execute_script('$(document).off("{0}")'.format(name))
+
+
 def try_until(work, errors=(Exception, ), timeout=10, interval=0.1):
     elapsed = 0
     while True:
         try:
-            work()
-            return
+            return work()
         except errors:
             time.sleep(interval)
             elapsed += interval
