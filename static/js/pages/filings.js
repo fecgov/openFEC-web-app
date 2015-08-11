62e07635c42da2410e2abb446d8b3bae58e59ba5
LindsayYoung <lindsayn.young@gsa.gov>
2015-08-11 10:57:14 -0400

    - Adding friendly document name  - Sorting most recent first  - link not working yet

--- static/js/pages/filings.js
+++ static/js/pages/filings.js
@@ -9,13 +9,14 @@ var tables = require('../modules/tables');
 
 var columns = [
   {
-    data: 'pdf_url',
+    data: 'document_description',
     className: 'all',
     orderable: false,
+    width: '20%',
     render: function(data, type, row, meta) {
       var anchor = document.createElement('a');
-      anchor.textContent = 'View filing';
-      anchor.setAttribute('href', data);
+      anchor.textContent = data;
+      anchor.setAttribute('href', row.pdf_url);
       anchor.setAttribute('target', '_blank');
       return anchor.outerHTML;
     }
@@ -23,8 +24,9 @@ var columns = [
   {data: 'committee_name', className: 'min-desktop', orderable: false},
   {data: 'candidate_name', className: 'min-desktop', orderable: false},
   {data: 'amendment_indicator', className: 'min-desktop'},
-  {data: 'report_type_full', className: 'min-desktop', orderable: false},
   tables.dateColumn({data: 'receipt_date', className: 'min-tablet'}),
+  // this would be better as a range of dates, with the title "Coverage Period"
+  tables.dateColumn({data: 'coverage_end_date', className: 'min-tablet', orderable: false}),
   tables.currencyColumn({data: 'total_receipts', className: 'min-tablet'}),
   tables.currencyColumn({data: 'total_disbursements', className: 'min-tablet'}),
   tables.currencyColumn({data: 'total_independent_expenditures', className: 'min-tablet'})
@@ -35,6 +37,6 @@ $(document).ready(function() {
   var $form = $('#category-filters');
   tables.initTable($table, $form, 'filings', {}, columns, tables.offsetCallbacks, {
     // Order by receipt date descending
-    order: [[5, 'desc']],
+    order: [[4, 'desc']],
   });
 });
