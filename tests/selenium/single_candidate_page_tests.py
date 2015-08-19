c4b293794b3e0e195041bd855b61b0ca348199d0
Joshua Carp <jm.carp@gmail.com>
2015-08-19 15:42:07 -0400

    Update integration tests.

--- tests/selenium/single_candidate_page_tests.py
+++ tests/selenium/single_candidate_page_tests.py
@@ -1,5 +1,3 @@
-import unittest
-
 from .base_test_class import BaseTest
 
 from tests.selenium import utils
@@ -9,24 +7,16 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 class SingleCandidatePageTests(BaseTest):
 
-    def setUp(self):
-        self.url = self.base_url + '/candidate/H0OH08029'
-
     def testSingleCandidatePageLoads(self):
         self.driver.get(self.base_url + '/candidates')
         utils.wait_for_event(self.driver, 'draw.dt', 'draw')
         self.driver.find_element_by_css_selector('a[data-category="candidate"]').click()
 
-    @unittest.skip('No principal committee links in test subset')
-    def testCommitteeLink(self):
-        self.driver.get(self.url)
-        self.assertTrue(self.elementExistsByXPath('//a[contains(@href, "committee/")]'))
-        link = self.driver.find_element_by_xpath('//a[contains(@href, "committees/")]')
-        self.assertEqual(link.text, 'OBAMA FOR AMERICA')
+    def test_glossary_load_from_term(self):
+        self.driver.get(self.base_url + '/candidates')
+        utils.wait_for_event(self.driver, 'draw.dt', 'draw')
+        self.driver.find_element_by_css_selector('a[data-category="candidate"]').click()
 
-
-    def testGlossaryLoadFromTerm(self):
-        self.driver.get(self.url)
         term = self.driver.find_element_by_class_name('term')
         term.click()
         glossary = self.getGlossary()
