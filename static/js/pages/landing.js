b6d12f49c823339812f299f53fbf5dd59a99c685
Joshua Carp <jm.carp@gmail.com>
2015-09-02 11:23:20 -0400

    Consolidate committee and candidate filer columns.
    Since at least one column is always null, consolidate the two columns to
    one, and show whichever value is not missing.
    
    [Resolves #470]

--- static/js/pages/landing.js
+++ static/js/pages/landing.js
@@ -9,19 +9,18 @@ var tables = require('../modules/tables');
 var columns = [
   tables.urlColumn('pdf_url', {data: 'document_description', className: 'all', orderable: false}),
   {
-    data: 'committee_name',
+    data: 'committee_id',
     className: 'min-desktop',
     orderable: false,
     render: function(data, type, row, meta) {
-      return tables.buildEntityLink(data, '/committee/' + row.committee_id + tables.buildCycle(row), 'committee');
-    },
-  },
-  {
-    data: 'candidate_name',
-    className: 'min-desktop',
-    orderable: false,
-    render: function(data, type, row, meta) {
-      return tables.buildEntityLink(data, '/candidate/' + row.candidate_id + tables.buildCycle(row), 'candidate');
+      var cycle = tables.buildCycle(row);
+      if (row.candidate_name) {
+        return tables.buildEntityLink(row.candidate_name, '/candidate/' + row.candidate_id + cycle, 'candidate');
+      } else if (row.committee_name) {
+        return tables.buildEntityLink(row.committee_name, '/committee/' + row.candidate_id + cycle, 'committee');
+      } else {
+        return '';
+      }
     },
   },
   tables.dateColumn({data: 'receipt_date', className: 'min-tablet', orderable: false}),
@@ -31,7 +30,7 @@ $(document).ready(function() {
   var $table = $('#results');
   tables.initTable($table, null, 'filings', {per_page: 10}, columns, tables.offsetCallbacks, {
     // Order by receipt date descending
-    order: [[3, 'desc']],
+    order: [[2, 'desc']],
     useFilters: false,
     dom: 't'
   });
