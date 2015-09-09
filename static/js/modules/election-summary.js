ac0e4c08c4feef5686fb8ec59e3e873ed7f287fa
Joshua Carp <jm.carp@gmail.com>
2015-09-09 16:26:06 -0400

    Prepend classes with "-js".
    h/t @msecret

--- static/js/modules/election-summary.js
+++ static/js/modules/election-summary.js
@@ -7,27 +7,27 @@ var URI = require('URIjs');
 
 var helpers = require('./helpers');
 
-function ElectionSummary(selector, opts) {
-  this.$elm = $(selector);
-  this.opts = opts;
-
-  this.$count = this.$elm.find('.count');
-  this.$receipts = this.$elm.find('.receipts');
-  this.$disbursements = this.$elm.find('.disbursements');
-
-  this.fetch();
-  this.$elm.find('a.button--election').attr('href', this.buildUrl());
-}
-
-ElectionSummary.prototype.buildUrl = function() {
-  var parts = [this.opts.office, this.opts.state, this.opts.district, this.opts.cycle]
+function buildUrl(opts) {
+  var parts = [opts.office, opts.state, opts.district, opts.cycle]
     .filter(function(part) {
       return !!part;
     })
     .concat('')
     .join('/');
   return URI(parts).toString();
-};
+}
+
+function ElectionSummary(selector, opts) {
+  this.$elm = $(selector);
+  this.opts = opts;
+
+  this.$count = this.$elm.find('.js-count');
+  this.$receipts = this.$elm.find('.js-receipts');
+  this.$disbursements = this.$elm.find('.js-disbursements');
+
+  this.fetch();
+  this.$elm.find('.js-election-url').attr('href', buildUrl(this.opts));
+}
 
 ElectionSummary.prototype.fetch = function() {
   var url = helpers.buildUrl(['elections', 'summary'], this.opts);
