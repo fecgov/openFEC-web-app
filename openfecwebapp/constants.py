c2b633c5910acf9b90fed35887848c6cd907edd8
emileighoutlaw <emily.barnes@gsa.gov>
2015-09-22 11:42:14 -0400

    Updates US
    Styles all U.S. as US for consistency

--- openfecwebapp/constants.py
+++ openfecwebapp/constants.py
@@ -69,7 +69,7 @@ parties_extended = OrderedDict([
     ('AMP', 'American Party'),
     ('APF', "American People's Freedom Party"),
     ('CIT', "Citizens' Party"),
-    ('CMP', 'Commonwealth Party of the U.S.'),
+    ('CMP', 'Commonwealth Party of the US'),
     ('COM', 'Communist Party'),
     ('CRV', 'Conservative Party'),
     ('CST', 'Constitutional'),
@@ -83,7 +83,7 @@ parties_extended = OrderedDict([
     ('ICD', 'Independent Conserv. Democratic'),
     ('IGD', 'Industrial Government Party'),
     ('IND', 'Independent'),
-    ('LAB', 'U.S. Labor Party'),
+    ('LAB', 'US Labor Party'),
     ('LBL', 'Liberal Party'),
     ('LBR', 'Labor Party'),
     ('LBU', 'Liberty Union Party'),
@@ -106,7 +106,7 @@ parties_extended = OrderedDict([
     ('THD', 'Theo-Dem'),
     ('TWR', 'Taxpayers Without Representation'),
     ('TX', 'Taxpayers'),
-    ('USP', "U.S. People's Party"),
+    ('USP', "US People's Party"),
 ])
 
 monthly_reports = OrderedDict([
