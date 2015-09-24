4292c3d8f22944e33b585f736d18148208edacb2
Joshua Carp <jm.carp@gmail.com>
2015-09-24 11:11:55 -0400

    Delete duplicate listener.

--- static/js/modules/tables.js
+++ static/js/modules/tables.js
@@ -40,11 +40,6 @@ $.fn.DataTable.Api.register('seekIndex()', function(length, start, value) {
   }
 });
 
-// Only show table after draw
-$(document.body).on('draw.dt', function() {
-  $('.datatable__container').css('opacity', '1');
-});
-
 function yearRange(first, last) {
   if (first === last) {
     return first;
