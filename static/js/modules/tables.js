f87cd768ace41662e69dcd3b7220478e01512eb8
Noah Manger <noah.manger@gsa.gov>
2016-04-01 15:18:11 -0700

    Changing class names to use new styles from styleguide cleanup

--- static/js/modules/tables.js
+++ static/js/modules/tables.js
@@ -211,7 +211,7 @@ function identity(value) {
 }
 
 var MODAL_TRIGGER_CLASS = 'js-panel-trigger';
-var MODAL_TRIGGER_HTML = '<button class="js-panel-button icon arrow--right">' +
+var MODAL_TRIGGER_HTML = '<button class="js-panel-button button--panel">' +
   '<span class="u-visually-hidden">Toggle details</span>' +
 '</button>';
 
