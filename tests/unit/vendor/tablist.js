68f33e89a23cda3dad46ce95e28d6a028c2b82d9
Evan Kroske <evankroske@google.com>
2016-04-03 19:57:23 -0700

    Adds test for tablist

--- tests/unit/vendor/tablist.js
+++ tests/unit/vendor/tablist.js
@@ -14,19 +14,45 @@ describe('tablist', function() {
   var fixture;
   before(function() {
     fixture = $('<div id="fixtures"></div>');
-    $('body').append(this.fixture);
+    $('body').append(fixture);
   });
 
   describe('init', function() {
     it('should show all tab panels if there are no tabs', function() {
       fixture.empty().append(
-        '<ul role="tablist" data-name="tab"></ul>' +
-        '<section role="tabpanel" aria-hidden></section>'
+        '<div class="tab-interface">' + 
+          '<ul role="tablist" data-name="tab"></ul>' +
+          '<section role="tabpanel" aria-hidden="true"></section>' +
+        '</div>'
       );
 
       tablist.init();
 
       expect($('[role="tabpanel"]').attr('aria-hidden')).to.be.undefined;
+    });
+
+    it('should show first tab if there\'s no query', function() {
+      fixture.empty().append(
+        '<div class="tab-interface">' + 
+          '<ul role="tablist" data-name="tab">' +
+            '<li><a role="tab" data-name="tab0" href="#section-0">0</a></li>' +
+            '<li><a role="tab" data-name="tab1" href="#section-1">1</a></li>' +
+          '</ul>' +
+          '<section id="section-0" role="tabpanel" aria-hidden="true">' +
+            '' +
+          '</section>' +
+          '<section id="section-1" role="tabpanel">' +
+            '' +
+          '</section>' +
+        '</div>'
+      );
+
+      tablist.init();
+
+      expect($('[data-name="tab0"]').attr('aria-selected')).to.be.eql('true');
+      expect($('[data-name="tab1"]').attr('aria-selected')).to.be.undefined;
+      expect($('#section-0').attr('aria-hidden')).to.be.undefined;
+      expect($('#section-1').attr('aria-hidden')).to.be.eql('true');
     });
   });
 });
