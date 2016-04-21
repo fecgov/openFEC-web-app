c9280702006fa827dfba369ea7e95b6f1cd002dd
Noah Manger <noah.manger@gsa.gov>
2016-04-21 11:29:07 -0700

    Fixing candidat office pages

--- static/js/pages/candidates-office.js
+++ static/js/pages/candidates-office.js
@@ -8,9 +8,6 @@ var tables = require('../modules/tables');
 var columnHelpers = require('../modules/column-helpers');
 var columns = require('../modules/columns');
 
-var FilterPanel = require('fec-style/js/filter-panel').FilterPanel;
-var filterTags = require('fec-style/js/filter-tags');
-
 var candidatesTemplate = require('../../templates/candidates-office.hbs');
 
 var columnGroups = {
@@ -21,15 +18,10 @@ var columnGroups = {
 
 $(document).ready(function() {
   var $table = $('#results');
-  var $widgets = $(tables.dataWidgets);
-  var $tagList = new filterTags.TagList({title: 'All records'}).$body;
-  var filterPanel = new FilterPanel();
-
   new tables.DataTable($table, {
     title: 'Candidate',
     path: ['candidates', 'totals'],
     query: {office: context.office.slice(0, 1).toUpperCase()},
-    panel: filterPanel,
     columns: columnGroups[context.office],
     useFilters: true,
     useExport: true,
@@ -38,5 +30,4 @@ $(document).ready(function() {
       afterRender: tables.modalRenderFactory(candidatesTemplate)
     }
   });
-  $widgets.find('.js-filter-tags').prepend($tagList);
 });
