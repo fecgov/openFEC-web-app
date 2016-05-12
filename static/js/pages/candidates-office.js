e0d1fa06741cc1a950a1a451c4c02e5c4785dbe0
Noah Manger <noah.manger@gsa.gov>
2016-05-12 15:48:09 -0700

    Adding committees and other info to details panels

--- static/js/pages/candidates-office.js
+++ static/js/pages/candidates-office.js
@@ -3,13 +3,13 @@
 /* global context */
 
 var $ = require('jquery');
+var _ = require('underscore');
 
 var tables = require('../modules/tables');
 var columnHelpers = require('../modules/column-helpers');
 var columns = require('../modules/columns');
-
-var FilterPanel = require('fec-style/js/filter-panel').FilterPanel;
-var filterTags = require('fec-style/js/filter-tags');
+var helpers = require('../modules/helpers');
+var URI = require('urijs');
 
 var candidatesTemplate = require('../../templates/candidates-office.hbs');
 
@@ -19,18 +19,64 @@ var columnGroups = {
   house: columnHelpers.getColumns(columns.candidateOffice, ['name', 'party', 'state', 'district', 'receipts', 'disbursements', 'trigger']),
 };
 
+var defaultSort = {
+  president: 2,
+  senate: 3,
+  house: 4
+};
+
+var renderModal = tables.modalRenderFactory(
+  candidatesTemplate,
+  function(row) {
+    // Parse all of the time-related variables
+    var query = URI.parseQuery(window.location.search);
+    var electionYear = query.election_year;
+    var cycle = query.cycle || query.election_year;
+    var electionFull = query.election_full === 'true' ? true : false;
+    var timePeriod = helpers.getTimePeriod(electionYear, cycle, electionFull, row.office);
+
+    // Build the URL and make a call to the history endpoint
+    var committeeUrl = helpers.buildUrl(
+      ['candidate', row.candidate_id, 'committees', 'history', cycle],
+      {'election_full': query.election_full}
+    );
+
+    return $.getJSON(committeeUrl).then(function(response) {
+      var results = response.results.length ?
+        response.results :
+        {};
+      // Sort the P and A committees
+      var principalCommittees = [],
+          authorizedCommittees = [];
+      results.forEach(function(result){
+        if (result.designation === 'P') {
+          principalCommittees.push(result);
+        } else if (result.designation === 'A') {
+          authorizedCommittees.push(result);
+        }
+      });
+      var committees = {
+        principal: principalCommittees,
+        authorized: authorizedCommittees
+      };
+      return _.extend({}, row, {committees: committees, query: query, time_period: timePeriod});
+    });
+  }
+);
+
 $(document).ready(function() {
   var $table = $('#results');
   new tables.DataTable($table, {
-    title: 'Candidate',
+    title: 'Candidates for ' + context.office,
     path: ['candidates', 'totals'],
     query: {office: context.office.slice(0, 1).toUpperCase()},
     columns: columnGroups[context.office],
+    order: [[defaultSort[context.office], 'desc']],
     useFilters: true,
     useExport: true,
     rowCallback: tables.modalRenderRow,
     callbacks: {
-      afterRender: tables.modalRenderFactory(candidatesTemplate)
+      afterRender: renderModal
     }
   });
 });
