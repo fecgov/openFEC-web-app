01367d80924bdab3d9d095fdf804cb1dc4b4ba3a
Aaron D Borden <adborden@a14n.net>
2016-05-19 11:35:13 -0700

    Include breadcrumbs on Legal search

--- openfecwebapp/views.py
+++ openfecwebapp/views.py
@@ -37,8 +37,14 @@ def render_legal_search_results(results, query, result_type):
 
 
 def render_legal_doc_search_results(results, query, result_type):
+    if result_type == 'aos':
+        document_type_display_name = 'Advisory Opinions'
+    else:
+        document_type_display_name = 'Documents'
+
     return render_template(
         'legal-doc-search-results.html',
+        document_type_display_name=document_type_display_name,
         results=results,
         result_type=result_type,
         query=query,
