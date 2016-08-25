a5bf3be58fa159aaf7e4cd7666185307eb267e26
Anthony Garvan <anthony.garvan@gsa.gov>
2016-08-25 09:08:39 -0500

    changing to assert syntax

--- tests/unit/test_legal_search.py
+++ tests/unit/test_legal_search.py
@@ -18,7 +18,7 @@ class TestLegalSearch(unittest.TestCase):
     @mock.patch.object(api_caller, 'load_legal_search_results')
     def test_no_query(self, load_legal_search_results):
         response = self.app.get('/legal/search/')
-        self.assertEqual(response.status_code, 200)
+        assert response.status_code == 200
         load_legal_search_results.assert_not_called()
 
     def test_search_type_regulations_redirects(self):
@@ -26,15 +26,15 @@ class TestLegalSearch(unittest.TestCase):
                 data={
                     'search': 'in kind donation',
                     'search_type': 'regulations'})
-        self.assertEqual(response.status_code, 302)
+        assert response.status_code == 302
 
         url = urlparse(response.location)
         query = parse_qs(url.query)
-        self.assertEqual(url.path, '/legal/search/regulations/')
-        self.assertIn('search', query)
-        self.assertIn('search_type', query)
-        self.assertEqual(query['search'], ['in kind donation'])
-        self.assertEqual(query['search_type'], ['regulations'])
+        assert url.path == '/legal/search/regulations/'
+        assert 'search' in query
+        assert 'search_type' in query
+        assert query['search'] == ['in kind donation']
+        assert query['search_type'] == ['regulations']
 
     @mock.patch.object(api_caller, 'load_legal_search_results')
     def test_search_universal(self, load_legal_search_results):
@@ -44,7 +44,7 @@ class TestLegalSearch(unittest.TestCase):
                     'search': 'in kind donation',
                     'search_type': 'all'})
 
-        self.assertEqual(response.status_code, 200)
+        assert response.status_code == 200
         load_legal_search_results.assert_called_once_with('in kind donation', 'all', limit=3)
 
     @mock.patch.object(api_caller, 'load_legal_search_results')
@@ -55,8 +55,9 @@ class TestLegalSearch(unittest.TestCase):
                     'search': 'in kind donation',
                     'search_type': 'regulations'})
 
-        self.assertEqual(response.status_code, 200)
-        load_legal_search_results.assert_called_once_with('in kind donation', 'regulations', offset=0)
+        assert response.status_code == 200
+        load_legal_search_results.assert_called_once_with('in kind donation',
+            'regulations', offset=0)
 
     @mock.patch.object(api_caller, 'load_legal_search_results')
     def test_search_advisory_opinions(self, load_legal_search_results):
@@ -65,9 +66,10 @@ class TestLegalSearch(unittest.TestCase):
                 data={
                     'search': 'in kind donation',
                     'search_type': 'advisory_opinions'})
-        self.assertEqual(response.status_code, 200)
+        assert response.status_code == 200
 
-        load_legal_search_results.assert_called_once_with('in kind donation', 'advisory_opinions', offset=0)
+        load_legal_search_results.assert_called_once_with('in kind donation',
+            'advisory_opinions', offset=0)
 
     @mock.patch.object(api_caller, 'load_legal_search_results')
     def test_search_pagination(self, load_legal_search_results):
@@ -77,8 +79,9 @@ class TestLegalSearch(unittest.TestCase):
                     'search': 'in kind donation',
                     'search_type': 'regulations',
                     'offset': 20})
-        self.assertEqual(response.status_code, 200)
-        load_legal_search_results.assert_called_once_with('in kind donation', 'regulations', offset=20)
+        assert response.status_code == 200
+        load_legal_search_results.assert_called_once_with('in kind donation',
+         'regulations', offset=20)
 
     @mock.patch.object(api_caller, 'load_legal_search_results')
     def test_search_statutes(self, load_legal_search_results):
@@ -87,7 +90,7 @@ class TestLegalSearch(unittest.TestCase):
                 data={
                     'search': 'in kind donation',
                     'search_type': 'statutes'})
-        self.assertEqual(response.status_code, 200)
+        assert response.status_code == 200
         load_legal_search_results.assert_called_once_with('in kind donation', 'statutes', offset=0)
 
     @mock.patch.object(api_caller, '_call_api')
