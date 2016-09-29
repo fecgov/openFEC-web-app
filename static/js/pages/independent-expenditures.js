9adea7d29b559d8a1d17146e8bedc94a56c174a6
Noah Manger <noah.manger@gsa.gov>
2016-09-28 17:40:44 -0700

    Adding TableSwitcher to IE filters

--- static/js/pages/independent-expenditures.js
+++ static/js/pages/independent-expenditures.js
@@ -4,6 +4,7 @@ var $ = require('jquery');
 
 var tables = require('../modules/tables');
 var columns = require('../modules/columns');
+var TableSwitcher = require('../modules/table-switcher').TableSwitcher;
 
 var expenditureTemplate = require('../../templates/independent-expenditures.hbs');
 
@@ -12,7 +13,7 @@ $(document).ready(function() {
   new tables.DataTable($table, {
     autoWidth: false,
     title: 'Independent expenditures',
-    path: 'schedules/schedule_e',
+    path: ['schedules', 'schedule_e'],
     columns: columns.independentExpenditures,
     paginator: tables.SeekPaginator,
     rowCallback: tables.modalRenderRow,
@@ -23,4 +24,17 @@ $(document).ready(function() {
       afterRender: tables.modalRenderFactory(expenditureTemplate)
     }
   });
+
+  new TableSwitcher('.js-table-switcher', {
+    summary: {
+      path: ['schedules', 'schedule_e']
+      // disableFilters: true,
+      // enabledFilters: ['committee_id', 'data_type', 'receipt_date'],
+      // hideColumns: '.hide-efiling',
+    },
+    notice: {
+      path: ['schedules', 'schedule_e', 'notice']
+      // hideColumns: '.hide-processed',
+    }
+  }).init();
 });
