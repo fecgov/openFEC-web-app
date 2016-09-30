abe74bad33d35107eaaea2362f6904ef81819b4f
Noah Manger <noah.manger@gsa.gov>
2016-09-30 14:49:18 -0700

    Reverts using TableSwitcher to filter between report types
    cc @jontours

--- static/js/pages/independent-expenditures.js
+++ static/js/pages/independent-expenditures.js
@@ -4,7 +4,6 @@ var $ = require('jquery');
 
 var tables = require('../modules/tables');
 var columns = require('../modules/columns');
-var TableSwitcher = require('../modules/table-switcher').TableSwitcher;
 
 var expenditureTemplate = require('../../templates/independent-expenditures.hbs');
 
@@ -24,17 +23,4 @@ $(document).ready(function() {
       afterRender: tables.modalRenderFactory(expenditureTemplate)
     }
   });
-
-  new TableSwitcher('.js-table-switcher', {
-    summary: {
-      path: ['schedules', 'schedule_e']
-    },
-    notice: {
-      path: ['schedules', 'schedule_e', 'notice'],
-      disableFilters: true,
-      enabledFilters: ['committee_id', 'cycle', 'data_type', 'candidate_id',
-                      'support_oppose_indicator', 'payee_name', 'min_amount',
-                      'max_amount', 'date'],
-    }
-  }).init();
 });
