e8ecca0f561c5f645b0fa457aaf872a58ea823e3
Vraj Mohan <radhakrishnan.vrajmohan@gsa.gov>
2017-02-22 05:29:22 -0800

    Fix tests to work with refactored AOs

--- tests/unit/test_legal_search.py
+++ tests/unit/test_legal_search.py
@@ -1,9 +1,6 @@
 import unittest
 from unittest import mock
 from urllib.parse import urlparse, parse_qs
-import codecs
-import json
-from collections import OrderedDict
 
 
 from openfecwebapp import api_caller
@@ -95,26 +92,15 @@ class TestLegalSearch(unittest.TestCase):
             None, None, None, None, None, None, None, None, offset=0)
 
     @mock.patch.object(api_caller, '_call_api')
-    def test_advisory_opinion_grouping(self, _call_api_mock):
-        _call_api_mock.return_value = {'advisory_opinions':
-                [{'no': 1, 'date': '2016'}, {'no': 1, 'date': '2015'},
-                 {'no': 2, 'date': '1999'}]}
-        results = api_caller.load_legal_search_results(query='president')
-        assert results == {'advisory_opinions': OrderedDict([(1,
-            [{'no': 1, 'date': '2016'}, {'no': 1, 'date': '2015'}]),
-            (2, [{'no': 2, 'date': '1999'}])]),
-            'limit': 20, 'offset': 0, 'advisory_opinions_returned': 3}
-
-    @mock.patch.object(api_caller, '_call_api')
     def test_result_counts(self, _call_api_mock):
-        _call_api_mock.return_value = {'advisory_opinions':
-            [{'no': 1, 'date': '2016'}, {'no': 1, 'date': '2015'},
-            {'no': 2, 'date': '1999'}], 'statutes': [{}] * 4,
+        _call_api_mock.return_value = {
+            'advisory_opinions': [{'no': 1, 'date': '2016'}, {'no': 2, 'date': '1999'}],
+            'statutes': [{}] * 4,
             'regulations': [{}] * 5}
         results = api_caller.load_legal_search_results(query='president')
 
         assert len(results['advisory_opinions']) == 2
-        assert results['advisory_opinions_returned'] == 3
+        assert results['advisory_opinions_returned'] == 2
         assert results['statutes_returned'] == 4
         assert results['regulations_returned'] == 5
 
