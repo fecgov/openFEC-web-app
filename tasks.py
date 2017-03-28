ebfe6d17199ceb12a340d28e1ea79b8155d7ebc4
LindsayYoung <lindsayn.young@gsa.gov>
2017-03-28 16:48:20 -0400

    Restore deploy rules

--- tasks.py
+++ tasks.py
@@ -70,10 +70,9 @@ def _detect_space(repo, branch=None, yes=False):
 
 
 DEPLOY_RULES = (
-    # ('prod', _detect_prod),
-    # ('stage', lambda _, branch: branch.startswith('release')),
-    # ('dev', lambda _, branch: branch == 'develop'),
-    ('dev', lambda _, branch: branch == 'feature/gov-cloud-support'),
+    ('prod', _detect_prod),
+    ('stage', lambda _, branch: branch.startswith('release')),
+    ('dev', lambda _, branch: branch == 'develop'),
 )
 
 
