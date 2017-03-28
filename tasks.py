f5de80334e3ff7fe96ffc236c1a4109f0e7a6725
LindsayYoung <lindsayn.young@gsa.gov>
2017-03-28 16:00:05 -0400

    Correct API url and deploy to dev

--- tasks.py
+++ tasks.py
@@ -70,9 +70,10 @@ def _detect_space(repo, branch=None, yes=False):
 
 
 DEPLOY_RULES = (
-    ('prod', _detect_prod),
-    ('stage', lambda _, branch: branch.startswith('release')),
-    ('dev', lambda _, branch: branch == 'develop'),
+    # ('prod', _detect_prod),
+    # ('stage', lambda _, branch: branch.startswith('release')),
+    # ('dev', lambda _, branch: branch == 'develop'),
+    ('feature/gov-cloud-support', lambda _, branch: branch == 'develop'),
 )
 
 
