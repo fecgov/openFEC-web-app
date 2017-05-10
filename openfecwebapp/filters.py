78653b8145361e496a77de86c41a24d3e41b883f
Noah Manger <noah.manger@gsa.gov>
2017-05-10 16:33:24 -0700

    Removing old code

--- openfecwebapp/filters.py
+++ openfecwebapp/filters.py
@@ -13,16 +13,19 @@ from openfecwebapp.app import app
 def get_context(c):
     return c
 
+
 @app.template_filter('currency')
 def currency_filter(num, grouping=True):
     if isinstance(num, (int, float)):
         return locale.currency(num, grouping=grouping)
     return None
 
+
 def ensure_date(value):
     if isinstance(value, (datetime.date, datetime.datetime)):
         return value
     return parse_date(value)
+
 
 @app.template_filter('date')
 def date_filter(value, fmt='%m/%d/%Y'):
@@ -30,14 +33,17 @@ def date_filter(value, fmt='%m/%d/%Y'):
         return None
     return ensure_date(value).strftime(fmt)
 
+
 @app.template_filter('ao_document_date')
 def ao_document_date(value):
     date = date_filter(value)
     return 'Not dated' if date == '01/01/1900' else date
 
+
 @app.template_filter('json')
 def json_filter(value):
     return json.dumps(value)
+
 
 @app.template_filter('filesize')
 def filesize_filter(value):
@@ -49,28 +55,6 @@ def filesize_filter(value):
 
     return '%d %s' % (value, units[unit])
 
-def _unique(values):
-    ret = []
-    for value in values:
-        if value not in ret:
-            ret.append(value)
-    return ret
-
-def _fmt_chart_tick(value):
-    try:
-        return parse_date(value).strftime('%m/%d/%Y')
-    except (AttributeError, ValueError):
-        return '?'
-
-@app.template_filter('fmt_chart_ticks')
-def fmt_chart_ticks(group, keys):
-    if not group or not keys:
-        return ''
-    if isinstance(keys, (list, tuple)):
-        values = [_fmt_chart_tick(group[key]) for key in keys]
-        values = _unique(values)
-        return ' – '.join(values)
-    return _fmt_chart_tick(group[keys])
 
 @app.template_filter()
 def fmt_year_range(year):
@@ -78,9 +62,11 @@ def fmt_year_range(year):
         return "{}–{}".format(year - 1, year)
     return None
 
+
 @app.template_filter()
 def fmt_state_full(value):
     return constants.states[value.upper()]
+
 
 @app.template_filter()
 def fmt_cycle_min_max(cycles):
@@ -88,9 +74,11 @@ def fmt_cycle_min_max(cycles):
         return '{}–{}'.format(min(cycles), max(cycles))
     return cycles[0]
 
+
 @app.template_filter()
 def get_max(list):
     return max(list)
+
 
 @app.template_filter()
 def get_min(list):
