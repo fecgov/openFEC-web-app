3f62bdd6af5074270231a4e626e6dc7af114c769
Noah Manger <noah.manger@gsa.gov>
2017-08-03 16:57:06 -0700

    Remove top lists

--- openfecwebapp/routes.py
+++ openfecwebapp/routes.py
@@ -41,22 +41,12 @@ def search():
             return views.render_search_results(results, query)
 
     else:
-        top_candidates_raising = api_caller.load_top_candidates('-receipts')
-        top_candidates_spending = api_caller.load_top_candidates('-disbursements')
-        top_pacs_raising = api_caller.load_top_pacs('-receipts')
-        top_pacs_spending = api_caller.load_top_pacs('-disbursements')
-        top_parties_raising = api_caller.load_top_parties('-receipts')
-        top_parties_spending = api_caller.load_top_parties('-disbursements')
+        top_candidates_raising = api_caller.load_top_candidates('-receipts', per_page=3)
         return render_template('landing.html',
             page='home',
             parent='data',
             dates=utils.date_ranges(),
             top_candidates_raising = top_candidates_raising['results'] if top_candidates_raising else None,
-            top_candidates_spending = top_candidates_spending['results'] if top_candidates_spending else None,
-            top_pacs_raising = top_pacs_raising['results'] if top_pacs_raising else None,
-            top_pacs_spending = top_pacs_spending['results'] if top_pacs_spending else None,
-            top_parties_raising = top_parties_raising['results'] if top_parties_raising else None,
-            top_parties_spending = top_parties_spending['results'] if top_parties_spending else None,
             title='Campaign finance data')
 
 
