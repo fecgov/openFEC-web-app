a3fdfcfcbd096d35492c237f2c0ea70eaa5e1c08
Noah Manger <noah.manger@gsa.gov>
2017-08-16 12:07:57 -0700

    More work on boolean field

--- static/js/legal/filters/BooleanTextFilter.js
+++ static/js/legal/filters/BooleanTextFilter.js
@@ -19,19 +19,23 @@ class BooleanTextFilter extends React.Component {
 
   /**
    * Handle "enter" events in the text input
+   * Get the value of the input and then clear the input
    */
   handleKeydown(e) {
-     if (e.keyCode === 13) {
-       this.handleTextChange(e.target.value);
-     }
-   }
+    if (e.keyCode === 13) {
+      this.handleTextChange(e.target.value);
+      e.target.value = '';
+    }
+  }
 
-   /**
-    * Handle clicking the submit button
-    */
-  handleSubmit() {
-    // Get value
-    // this.handleTextChange();
+  /**
+   * Handle clicking the submit button
+   * Get the value of the input and then clear the input
+   */
+  handleSubmit(e) {
+    e.preventDefault();
+    this.handleTextChange(e.target.previousSibling.value);
+    e.target.previousSibling.value = '';
   }
 
   /**
@@ -39,10 +43,8 @@ class BooleanTextFilter extends React.Component {
    */
   handleTextChange(value) {
     this.setState({
-      checkboxValues: this.state.checkboxValues.concat([this.parseCheckboxValues(value)]),
-    });
-
-    this.submitQuery();
+      checkboxValues: this.state.checkboxValues.concat([this.parseCheckboxValues(value)])
+    }, function () { this.submitQuery() });
   }
 
   /**
@@ -69,20 +71,36 @@ class BooleanTextFilter extends React.Component {
     let checkboxValues = this.state.checkboxValues;
     let index = Number(e.target.dataset.index) - 1;
     checkboxValues[index].checked = e.target.checked;
-    this.setState({checkboxValues: checkboxValues});
-    this.submitQuery();
+    this.setState({checkboxValues: checkboxValues}, function () { this.submitQuery() });
   }
 
   /**
    * Return an object with all the structured data for each checkbox
    */
   parseCheckboxValues(value, checked) {
-    let count = this.state ? this.state.checkboxValues.length + 1 : 0;
+    const count = this.state ? this.state.checkboxValues.length + 1 : 0;
+    let operator = 'OR';
+    let operatorLabel = 'or';
+    let label = value;
+
+    if (value.split('&').length > 1) {
+      operator = '&';
+      operatorLabel = 'and';
+      label = value.split('&')[1];
+    } else if (value.split('-').length > 1) {
+      operator = '-';
+      operatorLabel = 'excluding';
+      label = value.split('-')[1];
+    }
+
     return {
       index: count,
       name: this.props.name,
       key: 'checkbox-' + count,
+      label: label,
       value: value,
+      operator: operator,
+      operatorLabel: operatorLabel,
       checked: true,
       handleChange: this.handleCheckboxChange.bind(this)
     }
@@ -94,7 +112,7 @@ class BooleanTextFilter extends React.Component {
   combineValues() {
     let combinedValue = '';
     for (let i = 0; i < this.state.checkboxValues.length; i++) {
-      if (this.state.checkboxValues.checked) {
+      if (this.state.checkboxValues[i].checked) {
         let checkValue = this.state.checkboxValues[i].value;
         if (!combinedValue) {
           combinedValue = checkValue;
@@ -114,11 +132,13 @@ class BooleanTextFilter extends React.Component {
           return <div key={checkbox.key}>
             <input type="checkbox" id={`checkbox-${checkbox.name}-${checkbox.index}`} name={checkbox.name}
             onChange={checkbox.handleChange} checked={checkbox.checked} data-index={checkbox.index} />
-            <label htmlFor={`checkbox-${checkbox.name}-${checkbox.index}`}>{checkbox.value}</label>
+            <label htmlFor={`checkbox-${checkbox.name}-${checkbox.index}`}>
+              <strong>{checkbox.operatorLabel}</strong> {checkbox.label}
+            </label>
           </div>
         })}
         <div className="combo combo--search--mini">
-          <input value={this.state.textValue} id={this.props.name + "-filter"} type="text" name={this.props.name} className="combo__input"
+          <input id={this.props.name + "-filter"} type="text" name={this.props.name} className="combo__input"
               onChange={this.props.handleChange} onKeyDown={this.handleKeydown}/>
           <button className="combo__button button--search button--standard"
            onClick={this.handleSubmit}>
