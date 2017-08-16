573c292e11ce5283ac05d466724ec54664726f00
Noah Manger <noah.manger@gsa.gov>
2017-08-16 13:09:54 -0700

    Adding keyword modal

--- static/js/pages/legal.js
+++ static/js/pages/legal.js
@@ -2,5 +2,7 @@
 
 /* global require */
 var FilterPanel = require('fec-style/js/filter-panel').FilterPanel;
+var KeywordModal = require('../modules/keyword-modal').KeywordModal;
 
 new FilterPanel();
+new KeywordModal();
